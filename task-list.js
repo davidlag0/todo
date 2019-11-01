58b6b0261a4ba7665abf82a5dabdd1196218739f
David Laganière <davidlag@gmail.com>
2019-11-01 02:58:29 -0400

    Added modules for linting, enabled to check a task and made the SVG inlineto avoid shadow DOM issues.

--- task-list.js
+++ task-list.js
@@ -1,4 +1,4 @@
-let tasks = []
+const tasks = [];
 
 // Function to add a task in the task list.
 function addTask(text) {
@@ -12,23 +12,46 @@ function addTask(text) {
 
   const list = document.querySelector('.list');
 
-  list.insertAdjacentHTML('afterbegin', `
+  list.insertAdjacentHTML(
+    'afterbegin',
+    `
     <li class="list__task" data-key="${newTask.id}">
       <input id="${newTask.id}" type="checkbox" />
       <label for="${newTask.id}" class="list__task-label">
-        <svg><use xlink:href="#checkbox-unchecked"></use></svg>
+        <svg>
+          <rect class="checkmark" x="21" y="3" width="18" height="18" rx="1" transform="rotate(90 21 3)" />
+          <path class="checkmark" d="M6.66666 12.6667L9.99999 16L17.3333 8.66669" />
+          <rect class="checkbox" width="18" height="18" x="3" y="3" />
+        </svg>
       </label>
       <span>${newTask.text}</span>
       <button class="list__task-button">
-        <svg><use xlink:href="#cancel"></use></svg>
+        <svg>
+          <path d="M15.5355339 15.5355339L8.46446609 8.46446609M15.5355339 8.46446609L8.46446609 15.5355339" />
+          <path
+            d="M4.92893219,19.0710678 C1.02368927,15.1658249 1.02368927,8.83417511 4.92893219,4.92893219 C8.83417511,1.02368927 15.1658249,1.02368927 19.0710678,4.92893219 C22.9763107,8.83417511 22.9763107,15.1658249 19.0710678,19.0710678 C15.1658249,22.9763107 8.83417511,22.9763107 4.92893219,19.0710678 Z" />
+        </svg>
         <i class="material-icons orange600">face</i>
       </button>
     </li>
-  `);
+  `,
+  );
+}
+
+function toggleChecked(key) {
+  const taskIndex = tasks.findIndex((task) => task.id === Number(key));
+  tasks[taskIndex].checked = !tasks[taskIndex].checked;
+
+  const task = document.querySelector(`[data-key='${key}']`);
+  if (tasks[taskIndex].checked) {
+    task.classList.add('checked');
+  } else {
+    task.classList.remove('checked');
+  }
 }
 
 const form = document.querySelector('.task-form');
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   // To avoid the default form behavior of sending the content to the web server.
   event.preventDefault();
 
@@ -38,5 +61,13 @@ form.addEventListener('submit', event => {
   if (text !== '') {
     addTask(text);
     input.value = '';
+  }
+});
+
+const list = document.querySelector('.list');
+list.addEventListener('click', (event) => {
+  if (event.target.type === 'checkbox') {
+    const taskKey = event.target.parentElement.dataset.key;
+    toggleChecked(taskKey);
   }
 });
